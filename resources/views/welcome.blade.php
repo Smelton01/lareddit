d381a7a5d567d6a04f4e5cbae324ccfbed0a6c14
SimonJuba <scimail09@gmail.com>
2022-01-03 02:51:28 +0900

    barebones

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>
+        Welcome
+    </title>
+</head>
+<body style="background-color: #1111111">
+    <h1>PLEASE AGAIN TOMORrossw</h1>
+    {{ 2 * 34 + 99 }}
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet veritatis optio, placeat ex laudantium nam dolor, incidunt praesentium aspernatur numquam suscipit officia et provident rem perferendis, in maxime illum debitis?</p>
+</body>
+</html>
